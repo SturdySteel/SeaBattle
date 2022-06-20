e0ab74849a5772d471c7f7bcad0899c7a7bd82de
Andrey Bolohnov <hunt.zp@gmail.com>
2022-06-20 21:10:16 +0300

    2:add_print_battle_fileds

--- 20220619_seabattle/20220619_seabattle.cpp
+++ 20220619_seabattle/20220619_seabattle.cpp
@@ -6,7 +6,9 @@
 
 
 
-int rows{10},cols{ 10 };
+const int rows{10},cols{ 10 };
+
+
 
 enum ConsoleColor
 {
@@ -42,14 +44,20 @@ void showMessage(std::string message, ConsoleColor color = White, int pause = 0)
 	Sleep(pause);
 }
 
-void printSubField(int xCoord)
+int random(int min =0, int max = 9)
+{
+	return rand() % (max - min + 1) + min;
+}
+
+
+void printSubField(int** arr, int xCoord = 0, int yCoord = 0)
 {
 	char s = 65;
 	short maxSymbol = s + cols;
 
 	for (int i{}; i < rows + 1; ++i)
 	{
-		setPosition(xCoord, i + 1);
+		setPosition(xCoord, yCoord + i);
 		for (int j{}; j < cols + 2; ++j)
 		{
 			if (i == 0 && s < maxSymbol)
@@ -57,30 +65,64 @@ void printSubField(int xCoord)
 			else if (j == 0)
 				i < 10 ? std::cout << ' ' << i : std::cout << i;
 			else if (i != 0)
-				std::cout << "| ";
+			{
+				char t = arr[i - 1][j - 1];
+				t != 0 && j < cols + 1 ? std::cout << "|" << t : std::cout << "| ";
+			}
 			++s;
 		}
 	}
 }
 
-void printFields()
-{	
-	printSubField(0);
-	printSubField(cols*2 + 5);
+void printRules()
+{
+	setPosition(cols * 3 + 10,1);
 
+}
+
+void printBattleShip()
+{
+
+}
+
+void printFields(int*** arr )
+{	
+	printSubField(arr[0]);
+	printSubField(arr[1], cols * 2 + 5);
+	printSubField(arr[2], 0, rows + 3);
+	printSubField(arr[3], cols * 2 + 5, rows + 3);
+}
+
+int** genBattleField()
+{
+	int** arr = new int* [rows];
+
+	for (int i{}; i < rows; ++i)
+		arr[i] = new int[cols] {};
+		
+	return arr;
 }
 
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
+		
+	int** humanFieldSelf = genBattleField();
+	int** humanFieldBattle = genBattleField();
+	int** aiFieldSelf = genBattleField();
+	int** aiFieldBattle = genBattleField();
 
-	//setPosition(0, 5);
+	int*** battleField = new int**[4]{ humanFieldSelf, humanFieldBattle, aiFieldSelf, aiFieldBattle };
 
-    //showMessage("Hello World!", Red, 1000);
-	printFields();
+	humanFieldSelf[0][2] = 72;
+	humanFieldBattle[3][3] = 72;
+	aiFieldSelf[5][5] = 72;
+	aiFieldBattle[9][9] = 72;
+
+	printFields(battleField);
+	
 
 	std::cout << "\n\n";
-
-    return 0;
+	return 0;
 }
